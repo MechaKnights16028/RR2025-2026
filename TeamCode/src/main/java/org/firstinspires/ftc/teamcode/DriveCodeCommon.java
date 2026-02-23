3126bb681bce4923b74f576f271136c489c2af5c
MechaKnights <113733034+MechaKnights@users.noreply.github.com>
2026-02-23 15:07:52 -0800

    alliance determination by button and limelight used to determine exact position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeCommon.java
@@ -24,7 +24,7 @@ public class DriveCodeCommon extends LinearOpMode {
     int GREEN_RED_MAX = 70;
     int GREEN_BLUE_MAX = 70;
 
-    public static double IDEAL_SHOOT_DISTANCE = 48.0;
+    public static double IDEAL_SHOOT_DISTANCE = 109.0;
     public static double DISTANCE_TOLERANCE = 3.0;
     public static double ANGLE_TOLERANCE = 2.0;
     public static double ALIGN_ROTATE_GAIN = 0.02;
