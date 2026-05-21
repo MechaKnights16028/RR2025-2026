f779fc2721e0a8ed0b121dd080376028c0a145d0
MechaKnights <113733034+MechaKnights@users.noreply.github.com>
2026-05-21 14:14:33 -0600

    probably works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAutoRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAutoRoadrunner.java
@@ -82,8 +82,8 @@ public class blueAutoRoadrunner extends LinearOpMode {
                 if (startTime < 0) startTime = System.currentTimeMillis();
                 intake1.setPower(-0.5);
                 intake2.setPower(-0.25);
-                launcher1.setVelocity(-150);
-                launcher2.setVelocity(-150);
+                launcher1.setVelocity(-250);
+                launcher2.setVelocity(-250);
                 pusherWheel.setPower(0.1);
                 return System.currentTimeMillis() - startTime < 2500;
             }
